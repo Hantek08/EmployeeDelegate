9184e8db01b39a46d13d72b6449be0cbff5a2859
Hanna Tekie <hantek90@gmail.com>
2022-03-16 13:22:57 +0100

    Add project files.

---
+++ EmployeeDelegate/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDelegate
+{ 
+    delegate bool PromotDelegate(Employee emp);
+    
+    internal class Employee
+    {
+        //public static List<Employee> employees1 = new List<Employee>();
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Experience { get; set; }
+        public int Salary { get; set; }
+
+        //public Employee(int id, string name, int experience, int salary)
+        //{
+        //    this.ID = id;
+        //    this.Name = name;
+        //    this.Experience = experience;
+        //    this.Salary = salary;
+        //}
+   
+        public void PrintPromotedEmployee(List<Employee> employees, PromotDelegate promoted)
+        {
+            
+            foreach (var employee in employees)
+            {
+                if(promoted(employee))
+                {
+                    Console.WriteLine(employee.Name);
+                }
+                
+            }
+        }
+
+
+    }
+}
---
+++ EmployeeDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeDelegate;
+
+internal class Program
+{
+    
+    static void Main(string[] args)
+    {
+        //create some employee and store in a List
+       List<Employee> employeesList = new List<Employee>() {
+        new Employee(){ ID = 1, Name = "Emp1", Experience = 5 ,Salary = 35000},
+        new Employee(){ ID = 2, Name = "Emp2", Experience = 10, Salary = 40000},
+        new Employee() { ID = 3, Name = "Emp3", Experience = 2, Salary = 25000},
+        new Employee(){ ID = 4, Name = "Emp4", Experience = 6, Salary = 27000}
+        };
+
+     Employee employee = new Employee();
+        
+        employee.PrintPromotedEmployee(employeesList, Promot);
+
+    }
+    public static bool Promot(Employee emp)
+    {
+          if(emp.Experience > 5)
+            {
+                return true;
+            }
+               
+              return false; 
+    }
+}
